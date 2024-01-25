f4bf1ade44e8210e45cb01b599e8555e30eb760c
Adnan Umer <u.adnan@outlook.com>
2024-01-25 14:24:18 +0500

    chore: lint

--- splinter/apps/group/migrations/0001_initial.py
+++ splinter/apps/group/migrations/0001_initial.py
@@ -23,11 +23,18 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=255, unique=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='created_groups', to=settings.AUTH_USER_MODEL)),
+                (
+                    'created_by',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name='created_groups',
+                        to=settings.AUTH_USER_MODEL
+                    )
+                ),
             ],
             options={
                 'db_table': 'groups',
-                'ordering': ('name',),
+                'ordering': ('name', ),
             },
         ),
         migrations.CreateModel(
@@ -35,8 +42,20 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('group', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='memberships', to='group.group')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='group_memberships', to=settings.AUTH_USER_MODEL)),
+                (
+                    'group',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, related_name='memberships', to='group.group'
+                    )
+                ),
+                (
+                    'user',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name='group_memberships',
+                        to=settings.AUTH_USER_MODEL
+                    )
+                ),
             ],
             options={
                 'db_table': 'group_memberships',
@@ -46,6 +65,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='group',
             name='members',
-            field=models.ManyToManyField(related_name='+', through='group.GroupMembership', to=settings.AUTH_USER_MODEL),
+            field=models.ManyToManyField(
+                related_name='+', through='group.GroupMembership', to=settings.AUTH_USER_MODEL
+            ),
         ),
     ]
