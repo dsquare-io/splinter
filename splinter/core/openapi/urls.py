f8eaadc4c9b124f6fc867e3dcaaa911550a6d742
Adnan Umer <u.adnan@outlook.com>
2024-01-29 01:04:34 +0500

    feat: Add API docs using scaler

--- splinter/core/openapi/urls.py
+++ splinter/core/openapi/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 import splinter.core.openapi.extesions  # NOQA: Initialize extensions
@@ -6,5 +7,6 @@ import splinter.core.openapi.extesions  # NOQA: Initialize extensions
 app_name = 'openai'
 urlpatterns = [
     path('swagger/', SpectacularSwaggerView.as_view(url_name='openapi:schema')),
+    path('docs', TemplateView.as_view(template_name='openapi/docs.html')),
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
 ]
