ce74b434be74b23ba76100c8ade624bbd916457c
nmanumr <nmanumr@gmail.com>
2024-02-18 01:22:28 +0500

    fix (ui): Add authentication check to dashboard route
    The commit introduces a check for authentication status in the dashboard route. If a user is logged out, they will be redirected to the login page. This helps to protect the dashboard data by ensuring only authenticated users can view it.

--- ui/src/routes/_dashboard.tsx
+++ ui/src/routes/_dashboard.tsx
@@ -1,12 +1,16 @@
+import useAuth, { AuthStatus } from '@/hooks/useAuth.ts';
 import BottomNav from '@components/BottomNav.tsx';
 import Sidebar from '@components/Sidebar.tsx';
-import {Outlet, createFileRoute} from '@tanstack/react-router';
+import { Outlet, createFileRoute, Navigate } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_dashboard')({
   component: DashboardLayout,
 });
 
 function DashboardLayout() {
+  const {status} = useAuth();
+  if (status === AuthStatus.LOGGED_OUT) return <Navigate to="/auth/login" />;
+
   return (
     <div className="flex h-full flex-col md:contents">
       <main className="flex-1 overflow-auto  md:ms-60  md:h-full">
