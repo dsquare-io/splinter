161a87caffea23944ced9bde72cde5a500f985b8
nmanumr <nmanumr@gmail.com>
2024-02-25 00:52:45 +0500

    feat (ui): Remove SingleEntryItem component and modify friend details view
    The SingleEntryItem component has been deleted as it was no longer needed. The view for friend details has been modified to include a monthly breakdown of expenses. This breakdown groups expenses by month and provides a detailed summary for each transaction including the payee and outstanding balances.

--- ui/src/routes/_dashboard/friends/$friend.tsx
+++ ui/src/routes/_dashboard/friends/$friend.tsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx';
+import groupBy from 'just-group-by';
 import {Fragment} from 'react';
 
 import {BanknotesIcon, Cog8ToothIcon} from '@heroicons/react/16/solid';
 import {ChevronLeftIcon} from '@heroicons/react/24/solid';
 import {Link, createFileRoute} from '@tanstack/react-router';
+import {format} from 'date-fns';
 
 import {ApiRoutes} from '@/api-types';
 import Currency from '@/components/Currency.tsx';
@@ -18,8 +20,13 @@ function RootComponent() {
   const {friend: username} = Route.useParams();
 
   const {data} = useApiQuery(ApiRoutes.FRIEND_DETAIL, {username});
+  const {data: friendExpenseList} = useApiQuery(ApiRoutes.FRIEND_EXPENSE_LIST, {username});
 
   if (!data) return null;
+
+  const monthlyActivity = Object.entries(
+    groupBy(friendExpenseList?.results ?? [], (activity) => activity.datetime.split('-').slice(0, 2).join('-') + '-01')
+  );
 
   return (
     <div>
@@ -95,6 +102,56 @@ function RootComponent() {
             Settings
           </Button>
         </div>
+      </div>
+
+      <div className="my-2">
+        {monthlyActivity.map(([month, expenses]) => (
+          <div key={month}>
+            <h3 className="sticky top-[46px] bg-gray-50/70 pb-2 pt-4 text-sm text-neutral-500 backdrop-blur">
+              {format(new Date(month), 'MMM YYY')}
+            </h3>
+            <div>
+              {expenses.map((expense) => (
+                <div
+                  className="flex items-center gap-x-4 py-3"
+                  key={expense.uid}
+                >
+                  <div className="flex flex-col items-center">
+                    <p className="text-[8px] uppercase">{format(new Date(month), 'MMM')}</p>
+                    <p className="text-base uppercase tabular-nums text-gray-500">
+                      {format(new Date(month), 'dd')}
+                    </p>
+                  </div>
+
+                  <div className="flex-1">
+                    <p className="text-gray-900">
+                      {expense.expenses.length === 1
+                        ? expense.expenses[0].description
+                        : `${expense.expenses.length} Items`}
+                    </p>
+                    <div className="text-sm text-gray-500">{expense.paidBy?.fullName} paid</div>
+                  </div>
+
+                  <div>
+                    {+(expense.outstandingBalance ?? 0) === 0 ? (
+                      <div className="text-xs text-gray-400">Not involved</div>
+                    ) : (
+                      <div className="-mt-1 text-right text-sm">
+                        <div className="text-xs text-gray-400">
+                          {parseFloat(expense.outstandingBalance ?? '0') > 0 ? 'You lent' : 'You borrowed'}
+                        </div>
+                        <Currency
+                          currency="PKR"
+                          value={expense.outstandingBalance ?? '0'}
+                        />
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
