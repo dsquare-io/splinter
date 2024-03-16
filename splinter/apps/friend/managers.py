451f52f31ff14809a69ad04ed9e344137f61e3ce
Adnan Umer <u.adnan@outlook.com>
2024-03-17 02:53:04 +0500

    refactor: Avoid code duplication in FriendshipManager

--- splinter/apps/friend/managers.py
+++ splinter/apps/friend/managers.py
@@ -9,10 +9,7 @@ if TYPE_CHECKING:
 
 
 class FriendshipManager(Manager):
-    def is_friend_with(self, user_a: User, user_b: User) -> bool:
-        return self.filter(Q(user_a=user_a, user_b=user_b) | Q(user_a=user_b, user_b=user_a)).exists()
-
-    def get_user_friends(self, user: Union[User, int], include_self: bool = False) -> QuerySet:
+    def get_user_friends(self, user: Union[int, User], include_self: bool = False) -> QuerySet:
         target_id = user if isinstance(user, int) else user.pk
         qs = self.filter(Q(user_a=OuterRef('pk'), user_b=target_id) | Q(user_a=target_id, user_b=OuterRef('pk')))
 
@@ -22,5 +19,14 @@ class FriendshipManager(Manager):
 
         return User.objects.filter(q)
 
-    def of(self, user_a: User, user_b: User) -> 'Friendship':
-        return self.get(Q(user_a=user_a, user_b=user_b) | Q(user_a=user_b, user_b=user_a))
+    def _of(self, user_a: Union[int, User], user_b: Union[int, User]) -> QuerySet:
+        user_a_id = user_a if isinstance(user_a, int) else user_a.pk
+        user_b_id = user_b if isinstance(user_b, int) else user_b.pk
+
+        return self.filter(Q(user_a_id=user_a_id, user_b_id=user_b_id) | Q(user_a_id=user_b_id, user_b_id=user_a_id))
+
+    def of(self, user_a: Union[int, User], user_b: Union[int, User]) -> 'Friendship':
+        return self._of(user_a, user_b).get()
+
+    def is_friend_with(self, user_a: User, user_b: User) -> bool:
+        return self._of(user_a, user_b).exists()
