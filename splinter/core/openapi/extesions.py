d1ec677a2fb981786647b2f0ff1cc0ecc1208026
Adnan Umer <u.adnan@outlook.com>
2024-03-21 21:46:08 +0500

    feat: PolymorphicSerializer

--- splinter/core/openapi/extesions.py
+++ splinter/core/openapi/extesions.py
@@ -1,4 +1,10 @@
+from drf_spectacular.contrib.rest_polymorphic import (
+    PolymorphicSerializerExtension as PolymorphicSerializerExtensionBase,
+)
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
+from drf_spectacular.plumbing import ResolvedComponent, is_patched_serializer
+
+from splinter.core.serializers import PolymorphicSerializer
 
 
 class UserAccessTokenAuthenticationScheme(OpenApiAuthenticationExtension):
@@ -11,4 +17,38 @@ class UserAccessTokenAuthenticationScheme(OpenApiAuthenticationExtension):
             'scheme': 'bearer',
             'description': 'User access token',
             'bearerFormat': 'JWT',
+        }
+
+
+class PolymorphicSerializerExtension(PolymorphicSerializerExtensionBase):
+    target_class = f'{PolymorphicSerializer.__module__}.{PolymorphicSerializer.__name__}'
+
+    def map_serializer(self, auto_schema, direction):
+        sub_components = {}
+        serializer: PolymorphicSerializer = self.target
+
+        for discriminator, sub_serializer in serializer.serializer_mapping.items():
+            sub_serializer.partial = serializer.partial
+            component = auto_schema.resolve_serializer(sub_serializer, direction)
+            if not component:
+                # rebuild a virtual schema-less component to model empty serializers
+                component = ResolvedComponent(
+                    name=auto_schema._get_serializer_name(sub_serializer, direction),
+                    type=ResolvedComponent.SCHEMA,
+                    object='virtual',
+                )
+            typed_component = self.build_typed_component(
+                auto_schema=auto_schema,
+                component=component,
+                resource_type_field_name=serializer.discriminator_field,
+                patched=is_patched_serializer(sub_serializer, direction),
+            )
+            sub_components[discriminator] = typed_component.ref
+
+        return {
+            'oneOf': list(sub_components.values()),
+            'discriminator': {
+                'propertyName': serializer.discriminator_field,
+                'mapping': {discriminator: ref['$ref'] for discriminator, ref in sub_components.items()},
+            },
         }
--- splinter/core/serializers.py
+++ splinter/core/serializers.py
@@ -24,3 +24,53 @@ class ObjectSerializer(serializers.Serializer):
     @extend_schema_field(serializers.CharField(help_text='String representation of object'))
     def get_obj_value(self, obj) -> str:
         return str(obj)
+
+
+class PolymorphicSerializer(serializers.Serializer):
+    discriminator_field = 'type'
+
+    serializer_mapping = {}
+
+    def get_serializer_class(self, discriminator: str) -> type[serializers.Serializer]:
+        return self.serializer_mapping[discriminator]
+
+    def get_serializer(self, discriminator: str, **kwargs) -> serializers.Serializer:
+        serializer_class = self.get_serializer_class(discriminator)
+        kwargs['context'] = self.context
+        return serializer_class(**kwargs)
+
+    def get_fields(self):
+        return {
+            self.discriminator_field: serializers.CharField(),
+        }
+
+    def get_discriminator(self, instance) -> str:
+        raise NotImplementedError()
+
+    def to_representation(self, value):
+        discriminator = self.get_discriminator(value)
+        serialized = self.get_serializer(discriminator, instance=value).to_representation(value)
+        serialized[self.discriminator_field] = discriminator
+        return serialized
+
+    def to_internal_value(self, data):
+        discriminator = super().to_internal_value(data)[self.discriminator_field]
+
+        serializer = self.get_serializer(discriminator, data=data).data
+        setattr(self, '_serializer', serializer)
+
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data
+
+    def save(self, **kwargs):
+        assert hasattr(self, '_errors'), 'You must call `.is_valid()` before calling `.save()`.'
+
+        assert not self.errors, 'You cannot call `.save()` on a serializer with invalid data.'
+
+        assert not hasattr(self, '_data'), (
+            "You cannot call `.save()` after accessing `serializer.data`."
+            "If you need to access data before committing to the database then "
+            "inspect 'serializer.validated_data' instead. "
+        )
+
+        return getattr(self, '_serializer').save(**kwargs)
