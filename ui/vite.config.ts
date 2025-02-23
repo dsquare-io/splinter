9e190659d55060d6ad57494f32144a75de437603
Nauman Umer <nmanumr@gmail.com>
2025-02-23 23:48:35 +0500

    Add PWA support and generate related assets
    Integrated Vite PWA plugin to enable progressive web app functionality, including manifest and service worker setup. Added asset generation for icons and updated HTML metadata for PWA compliance. Introduced new scripts and dependencies to manage PWA assets.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import {TanStackRouterVite} from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import {defineConfig, loadEnv} from 'vite';
-import tailwindcss from "@tailwindcss/vite";
+import {VitePWA} from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -13,6 +15,41 @@ export default defineConfig(({mode}) => {
       react(),
       TanStackRouterVite(),
       tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Splinter',
+          short_name: 'Splinter',
+          description: 'Opensource expense tracking',
+          theme_color: '#267360',
+          "icons": [
+            {
+              "src": "pwa-64x64.png",
+              "sizes": "64x64",
+              "type": "image/png"
+            },
+            {
+              "src": "pwa-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+            },
+            {
+              "src": "pwa-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+            },
+            {
+              "src": "maskable-icon-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "maskable"
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
     ],
     resolve: {
       alias: {
